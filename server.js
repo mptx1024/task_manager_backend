86e3ef794cc329e4bd8e7e06decb76c7cfd4f6f8
Frank Jia <frankjzy@gmail.com>
2023-02-09 16:51:57 -0600

    refactored firebase private key format

--- server.js
+++ server.js
@@ -16,17 +16,16 @@ app.use(express.json());
 app.use(cors());
 
 const useEmulator = process.env.NODE_ENV === 'production' ? false : true;
+// const useEmulator = false;
 
 if (useEmulator) {
     process.env['FIREBASE_AUTH_EMULATOR_HOST'] = '127.0.0.1:9099';
 }
 
 const defaultApp = admin.initializeApp({
-    // or
-    // credential: admin.applicationDefault(),
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
 });
