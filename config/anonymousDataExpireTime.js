ee289748a591b76a64d3732387f41880f0a13ffb
Frank Jia <frankjzy@gmail.com>
2023-02-15 19:11:44 -0600

    refactored

--- config/anonymousDataExpireTime.js
+++ config/anonymousDataExpireTime.js
@@ -1,7 +1,8 @@
 const expireTime = () => {
+    const expireInDays = 2;
     let date = new Date();
     // date.setMinutes(date.getMinutes() + 5); // 1440 mins === one day
-    date.setMinutes(date.getDate() + 2); // expire in x days
+    date.setTime(date.getTime() + 86400000 * expireInDays); // expire in x days
     return date;
 };
 
